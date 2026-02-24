a7c56148fc0abd109a5c4f70de3cd8a4cecd9073
Sander Zirel <sanderzirel@gmail.com>
2026-02-24 11:03:48 +0200

    Initial project setup

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+  onMounted(() => {
+    fetch("http://localhost:8080/")
+      .then(res => res.text())
+      .then(data => {
+        console.log(data)
+      })
+  })
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<style scoped></style>
